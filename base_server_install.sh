209ca12c35204465a242b56eb0d59a46d3e936d0
Gabor Egyed <gabor.egyed@vertis.com>
2016-08-23 14:21:31 +0200

    delete locale row

--- base_server_install.sh
+++ base_server_install.sh
@@ -1,4 +1,3 @@
-export LC_ALL="en_US.UTF-8"
 sudo apt-get install -y vim git python-pip curl
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list
