a7c89045affc272ee2462f1a6ab2960d8da5d9c1
Gabor Egyed <gabor.egyed@vertis.com>
2016-11-25 14:19:01 +0100

    base user set same stuff and gitprompt and bash complation alias too

--- base_server_install.sh
+++ base_server_install.sh
@@ -8,8 +8,16 @@ pip install docker-compose
 mkdir -p /etc/skel/
 sh -c "echo 'alias dc=docker-compose
 alias d=docker' > /etc/skel/.bash_aliases"
+sh -c "echo 'alias dc=docker-compose
+alias d=docker' > ~/.bash_aliases"
 mkdir -p /etc/skel/.ssh
+mkdir -p ~/.ssh
+chmod 700 /etc/skel/.ssh
+chmod 700 ~/.ssh
 sh -c "echo '' > /etc/skel/.ssh/authorized_keys"
+sh -c "echo '' > ~/.ssh/authorized_keys"
+chmod 640 /etc/skel/.ssh/authorized_keys
+chmod 640 ~/.ssh/authorized_keys
 sh -c "echo 'set nocompatible
 set autoindent
 set backup
@@ -34,6 +42,46 @@ filetype plugin on
 set modeline
 set ls=2
 colorscheme molokai' > /etc/skel/.vimrc"
+sh -c "echo 'set nocompatible
+set autoindent
+set backup
+set nu
+set smartindent
+set showmatch
+set textwidth=80
+set title
+set tabstop=4
+set shiftwidth=4
+set softtabstop=4
+set expandtab
+set paste
+set noswapfile
+set nobackup
+set nowritebackup
+syntax on
+set t_Co=256
+filetype on
+filetype indent on
+filetype plugin on
+set modeline
+set ls=2
+colorscheme molokai' > ~/.vimrc"
 rm -rf /etc/skel/.vim
+rm -rf ~/.vim
 git clone https://github.com/vertisfinance/molokai.git /etc/skel/.vim/
+git clone https://github.com/vertisfinance/molokai.git ~/.vim/
 curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose | tee /etc/bash_completion.d/docker-compose
+git clone https://github.com/richardbann/gitprompt.git /etc/skel/
+git clone https://github.com/richardbann/gitprompt.git ~/
+sh -c "echo 'complete -F _docker_compose dc >> /etc/skel/.bashrc'""
+sh -c "echo 'complete -F _docker_compose dc >> ~/.bashrc'""
+sh -c "echo 'set_bash_prompt(){
+    PS1="$(python [~/gitprompt/prompt.py)"
+}
+
+PROMPT_COMMAND=set_bash_prompt >> /etc/skel/.bashrc'"
+sh -c "echo 'set_bash_prompt(){
+    PS1="$(python [~/gitprompt/prompt.py)"
+}
+
+PROMPT_COMMAND=set_bash_prompt >> ~/.bashrc'"
